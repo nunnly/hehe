af708edc6d4ecca138abfcbf6538498740fcac76
Nunn <whktk@qq.com>
2014-09-19 16:58:47 +0800

    主要流程走了一边，有些更内核的东西，比如`require`模块没有去深入的了解，下一步开始分析application.js

--- bin/www.js
+++ bin/www.js
@@ -5,6 +5,7 @@ var app = require('../app');
 //设置settings里的port属性为3000
 app.set('port', process.env.PORT || 3000);
 //app.get('port')获取上面设置的这个端口号，在只有一个参数的情况下，app.get == app.set
+//TODO-me：分析 application.js
 var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port);
 });
